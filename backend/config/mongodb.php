8586be160b03792bfa909a484951568fca275b9a
Loann Marcel <loannmarcel@outlook.fr>
2026-09-18 21:07:45 +0200

    fix: utilise la variable MongoDB en production

--- backend/config/mongodb.php
+++ backend/config/mongodb.php
@@ -1,6 +1,6 @@
 <?php
 
-$mongoUri = $_ENV['MONGODB_URI'] ?? 'mongodb://127.0.0.1:27017';
+$mongoUri = getenv('MONGODB_URI') ?: 'mongodb://127.0.0.1:27017';
 
 try {
     $mongoManager = new MongoDB\Driver\Manager($mongoUri);
